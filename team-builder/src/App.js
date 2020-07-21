0a75b124c567f82005e697ab3b57ac64dfee3608
jonathan warner <j.m.warners@gmail.com>
2020-07-21 14:05:33 -0700

    got app and components constructed. Debugging'

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Form from './Form'
+import Member from './Member'
+
+
+const initialMembersList = {
+  name: 'Jonathan',
+  email: 'j.m.warners@gmail.com',
+  role: 'Student',
+}
+
+
+const InitialFormValues = {
+  name: '',
+  email: '',
+  role: '',
+}
+
+const fakeAxiosGet = () => {
+  return Promise.resolve({ status: 200, success: true, data: initialMembersList})
+}
+
+const fakeAxiosPost = (url, { name, email, role }) => {
+  const newMember = {name, email, role }
+  return Promise.resolve({ status: 200, success: true, data: newMember})
+}
+
 
 function App() {
+  const [members, setMembers] = useState([])
+
+  const [formValues, setFormValues] = useState(InitialFormValues)
+
+  const updateForm = (inputName, inputValue) => {
+    const updatedFormValues = { ...formValues, [inputName]: inputValue}
+    setFormValues(updatedFormValues)
+  }
+
+  const submitForm = () => {
+    const newMemberFromFormStateClean = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+  }
+
+  if (!newMember.name || !newMember.email || !newMember.role) return
+
+  fakeAxiosPost('fakeAPI.com', newMember)
+    .then (res => {
+      const memberFromAPI = res.data
+      setMembers([memberFromAPI, ...members])
+      setFormValues(InitialFormValues)
+    })
+
+  useEffect(() => {
+    fakeAxiosGet('fakeAPI.com').then(res => setMembers(res.data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="memberList">
+      <header><h1>Members:</h1></header>
+      <Form
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
+      
+      {
+        members.map(member => {
+          return (
+            <Member key={member.id} details={member} />
+          )
+        })
+      }
     </div>
   );
 }
---
+++ team-builder/src/Form.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export default function Form (props) {
+    const { values, update, submit } = props
+
+    const onChange = evt => {
+        const { name, value } = evt.target
+        update(name, value)
+    }
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    return (
+        <form className='form container'>
+            <div className='form-group submit'>
+                <h2>Add a Team Member</h2>
+                <button disabled={!values.name || !values.email || !values.role}>Submit</button>
+            </div>
+
+            <div className='form-group inputs'>
+                <h3>Team Member Information</h3>
+                <label htmlFor='nameInput'>Name:&nbsp;
+                    <input
+                    id='nameInput'
+                    name='name'
+                    type='text'
+                    placeholder='Enter member name'
+                    maxLength='30'
+                    value={values.name}
+                    onChange={onChange}
+                    />
+                </label>
+
+                <label htmlFor='emailInput'>Email:&nbsp;
+                    <input
+                    id='emailInput'
+                    name='email'
+                    type='text'
+                    placeholder='Enter email'
+                    maxLength='30'
+                    value={values.email}
+                    onChange={onChange}
+                    />
+                </label>
+
+                <label>Role:&nbsp;
+                    <select name='role' value={values.role} onChange={onChange}>
+                        <option value=''>Select a Role</option>
+                        <option value='student'>Student</option>
+                        <option value='tl'>Team Lead</option>
+                        <option value='sl'>Section Lead</option>
+                        <option value='instructor'>Instructor</option>
+                    </select>
+                </label>
+            </div>
+        </form>
+    )
+}
